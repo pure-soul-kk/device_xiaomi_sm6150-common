7209d3c73c1ee55a52f9c6c872056338cb5f5da4
pure-soul-kk <krishnakripa34567@gmail.com>
2023-07-07 09:09:39 +0530

    sm6150-common: init evox

--- sm6150.mk
+++ sm6150.mk
@@ -274,7 +274,7 @@ PRODUCT_PACKAGES += \
 # Overlay
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-pixys
+    $(LOCAL_PATH)/overlay-evolution
 
 PRODUCT_ENFORCE_RRO_TARGETS += *
 
