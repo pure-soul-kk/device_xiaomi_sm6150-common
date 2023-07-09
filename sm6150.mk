4d68feebb8e136d4ff59d6b4fd42891aab2c8044
pure-soul-kk <krishnakripa34567@gmail.com>
2023-07-09 18:41:32 +0530

    sm6150-common: init AwakenOS

--- sm6150.mk
+++ sm6150.mk
@@ -274,7 +274,7 @@ PRODUCT_PACKAGES += \
 # Overlay
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-evolution
+    $(LOCAL_PATH)/overlay-awaken
 
 PRODUCT_ENFORCE_RRO_TARGETS += *
 
