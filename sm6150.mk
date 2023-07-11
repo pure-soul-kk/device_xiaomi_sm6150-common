54ffa712315fbeb4745a4635494a94f04766ee1d
pure-soul-kk <krishnakripa34567@gmail.com>
2023-07-11 19:04:56 +0530

    sm6150-common: RisingOS Bringup

--- sm6150.mk
+++ sm6150.mk
@@ -274,7 +274,7 @@ PRODUCT_PACKAGES += \
 # Overlay
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-awaken
+    $(LOCAL_PATH)/overlay-lineage
 
 PRODUCT_ENFORCE_RRO_TARGETS += *
 
