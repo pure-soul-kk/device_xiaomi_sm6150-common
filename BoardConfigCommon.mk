0cff6bee124da4a4b5ce6ddb1d220c03c9a87440
pure-soul-kk <krishnakripa34567@gmail.com>
2023-07-23 08:46:31 +0530

    Revert "sm6150-common: Switch to adrian-clang"
    This reverts commit df9fb34219f102e6b641bdbef8c5e187fbbb9481.

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -75,11 +75,9 @@ BOARD_KERNEL_BASE := 0x00000000
 BOARD_KERNEL_IMAGE_NAME := Image.gz
 BOARD_KERNEL_PAGESIZE := 4096
 BOARD_KERNEL_SEPARATED_DTBO := true
-
 TARGET_KERNEL_SOURCE := kernel/xiaomi/sm6150
-TARGET_KERNEL_CLANG_VERSION := adrian
-TARGET_KERNEL_CLANG_PATH := $(shell pwd)/prebuilts/clang/host/linux-x86/adrian-clang
-
+TARGET_KERNEL_CLANG_VERSION := playground
+TARGET_KERNEL_CLANG_PATH := $(shell pwd)/prebuilts/clang/host/linux-x86/clang-playground
 BOARD_KERNEL_CMDLINE += androidboot.hardware=qcom
 BOARD_KERNEL_CMDLINE += androidboot.usbcontroller=a600000.dwc3
 BOARD_KERNEL_CMDLINE += service_locator.enable=1
