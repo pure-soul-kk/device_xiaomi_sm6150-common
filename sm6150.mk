b8a3a41ad537a5f8e9705906af416b34d729728a
pure-soul-kk <krishnakripa34567@gmail.com>
2024-07-11 10:31:25 +0530

    sm6150-common: blaze-ify

--- sm6150.mk
+++ sm6150.mk
@@ -338,7 +338,7 @@ PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH) \
     hardware/google/interfaces \
     hardware/google/pixel \
-    hardware/lineage/interfaces/power-libperfmgr \
+    hardware/blaze/interfaces/power-libperfmgr \
     hardware/qcom-caf/common/libqti-perfd-client \
     hardware/xiaomi
 
